ee4d784c48909c1ad547c837dcf9866c6827f4d7
lz.piotr <piotr@layerzerolabs.org>
2023-03-31 11:22:53 +0200

    chore: format helpers

--- packages/lz-sdk/src/utils/helpers.ts
+++ packages/lz-sdk/src/utils/helpers.ts
@@ -1,26 +1,23 @@
-import { ChainId, ChainKey, ChainStage } from '../enums'
-import { CHAIN_ID, CHAIN_KEY, CHAIN_STAGE } from '../constants'
+import { ChainId, ChainKey, ChainStage } from "../enums"
+import { CHAIN_ID, CHAIN_KEY, CHAIN_STAGE } from "../constants"
 
 export function getNetworkNameByEndpointId(endpointId: ChainId | number): ChainKey {
-  return CHAIN_KEY[endpointId as ChainId]
+    return CHAIN_KEY[endpointId as ChainId]
 }
 
 export function getEndpointIdByName(networkName: ChainKey | string): ChainId {
-  return CHAIN_ID[networkName as ChainKey]
+    return CHAIN_ID[networkName as ChainKey]
 }
 
 export function getNetworksForEnv(chainStage: string) {
-  if (chainStage === 'sandbox') {
-    chainStage = 'testnet_sandbox'
-  }
-  let networks: string[] = []
-  for (const chainKey in ChainKey) {
-    if (
-      ChainStage[chainStage.toUpperCase() as keyof typeof ChainStage] ==
-      CHAIN_STAGE[ChainKey[chainKey as keyof typeof ChainKey]]
-    ) {
-      networks.push(chainKey.toLowerCase().replace(/_/g, '-'))
+    if (chainStage === "sandbox") {
+        chainStage = "testnet_sandbox"
     }
-  }
-  return networks
+    let networks: string[] = []
+    for (const chainKey in ChainKey) {
+        if (ChainStage[chainStage.toUpperCase() as keyof typeof ChainStage] == CHAIN_STAGE[ChainKey[chainKey as keyof typeof ChainKey]]) {
+            networks.push(chainKey.toLowerCase().replace(/_/g, "-"))
+        }
+    }
+    return networks
 }
