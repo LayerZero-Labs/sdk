4b3823cea886f2f85ba88eea3d3d8080e3a4ff1a
lz.piotr <piotr@layerzerolabs.org>
2023-03-31 11:38:13 +0200

    chore: remove constants

--- packages/lz-sdk/src/utils/helpers.ts
+++ packages/lz-sdk/src/utils/helpers.ts
@@ -1,22 +1,22 @@
 import { ChainId, ChainKey, ChainStage } from "../enums"
-import { CHAIN_ID, CHAIN_KEY, CHAIN_STAGE } from "../constants"
 
+// @deprecated - use getChainKey
 export function getNetworkNameByEndpointId(endpointId: ChainId | number): ChainKey {
-    return CHAIN_KEY[endpointId as ChainId]
+    return getChainKey(endpointId)
 }
 
+// @deprecated - use getChainIdByChainKey
 export function getEndpointIdByName(networkName: ChainKey | string): ChainId {
-    return CHAIN_ID[networkName as ChainKey]
+    return getChainIdByChainKey(networkName as ChainKey)
 }
 
 export function getNetworksForEnv(chainStage: string) {
-    if (chainStage === "sandbox") {
-        chainStage = "testnet_sandbox"
-    }
-    let networks: string[] = []
-    for (const chainKey in ChainKey) {
-        if (ChainStage[chainStage.toUpperCase() as keyof typeof ChainStage] == CHAIN_STAGE[ChainKey[chainKey as keyof typeof ChainKey]]) {
-            networks.push(chainKey.toLowerCase().replace(/_/g, "-"))
+    const networks: ChainKey[] = []
+    for (const chainIdAsString in ChainId) {
+        const chainId = Number(chainIdAsString)
+        if (!Number.isFinite(chainId)) continue
+        if (getChainStage(chainId) === (chainStage as any)) {
+            networks.push(getChainKey(chainId))
         }
     }
     return networks
@@ -26,4 +26,26 @@ export function getChainStage(chainId: number): ChainStage {
     if (chainId < 10000) return ChainStage.MAINNET
     if (chainId < 20000) return ChainStage.TESTNET
     return ChainStage.TESTNET_SANDBOX
+}
+
+export function getChainKey(chainId: ChainId): ChainKey {
+    for (const [key, chainIdOrString] of Object.entries(ChainId)) {
+        if (chainIdOrString === chainId) {
+            //@ts-ignore
+            const chainKey: ChainKey = ChainKey[key]
+            if (chainKey) return chainKey
+        }
+    }
+    throw new Error(`No ChainKey for ${chainId}`)
+}
+
+export function getChainIdByChainKey(chainKey: ChainKey) {
+    for (const [key, value] of Object.entries(ChainKey)) {
+        if (value === chainKey) {
+            //@ts-ignore
+            const chainId: ChainId = ChainId[key]
+            if (chainId !== undefined) return chainId
+        }
+    }
+    throw new Error(`No ChainId for ${chainKey}`)
 }
